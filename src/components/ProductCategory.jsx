c48ebd22ff8ff945912f1070f0b9a595071f137e
adnanamiruddin <adnan.amiruddin34@gmail.com>
2023-12-18 11:35:33 +0800

    fix: Modify text on ProductCategory

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,36 +1,32 @@
 const ProductCategory = ({ category }) => {
-  const toPascalCase = (str) => {
-    return str.replace(/\b\w/g, (match) => match.toUpperCase());
-  };
-
   let categoryComponent;
 
   switch (category) {
     case "men's clothing":
       categoryComponent = (
-        <span className="bg-blue-100 text-blue-800 text-xs font-medium text-center py-0.5 px-2">
-          {toPascalCase(category)}
+        <span className="capitalize bg-blue-100 text-blue-800 text-xs font-medium text-center py-0.5 px-2">
+          {category}
         </span>
       );
       break;
     case "women's clothing":
       categoryComponent = (
-        <span className="bg-pink-100 text-pink-800 text-xs font-medium text-center py-0.5 px-2">
-          {toPascalCase(category)}
+        <span className="capitalize bg-pink-100 text-pink-800 text-xs font-medium text-center py-0.5 px-2">
+          {category}
         </span>
       );
       break;
     case "jewelery":
       categoryComponent = (
-        <span className="bg-yellow-100 text-yellow-800 text-xs font-medium text-center py-0.5 px-2">
-          {toPascalCase(category)}
+        <span className="capitalize bg-yellow-100 text-yellow-800 text-xs font-medium text-center py-0.5 px-2">
+          {category}
         </span>
       );
       break;
     case "electronics":
       categoryComponent = (
-        <span className="bg-green-100 text-green-800 text-xs font-medium text-center py-0.5 px-2">
-          {toPascalCase(category)}
+        <span className="capitalize bg-green-100 text-green-800 text-xs font-medium text-center py-0.5 px-2">
+          {category}
         </span>
       );
       break;
